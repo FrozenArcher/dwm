728810cbd5a9f664a85706a5f998a127444b30a0
FrozenArcher <yhy242989105@hotmail.com>
2023-07-04 18:38:33 +0800

    horizgrid gaps

--- dwm-src/horizgrid.c
+++ dwm-src/horizgrid.c
@@ -1,7 +1,7 @@
 void
 horizgrid(Monitor *m) {
 	Client *c;
-	unsigned int n, i;
+	unsigned int n, i, oe = enablegaps, ie = enablegaps, iv, ih, ov, oh, cw, cx;
 	int w = 0;
 	int ntop, nbottom = 0;
 
@@ -10,23 +10,40 @@ horizgrid(Monitor *m) {
 
 	if(n == 0)
 		return;
-	else if(n == 1) { /* Just fill the whole screen */
+
+	if (smartgaps == n) {
+		oe = 0; // outer gaps disabled
+	}
+
+    iv = m->gappiv * ie;
+    ih = m->gappih * ie;
+    ov = m->gappov * oe;
+    oh = m->gappoh * oe;
+
+	if(n == 1) { /* Just fill the whole screen */
 		c = nexttiled(m->clients);
-		resize(c, m->wx, m->wy, m->ww - (2*c->bw), m->wh - (2*c->bw), False);
+		resize(c, m->wx + oh, m->wy + ov, m->ww - (2*c->bw) - (2*oh), m->wh - (2*c->bw) - (2*ov), False);
 	} else if(n == 2) { /* Split vertically */
 		w = m->ww / 2;
 		c = nexttiled(m->clients);
-		resize(c, m->wx, m->wy, w - (2*c->bw), m->wh - (2*c->bw), False);
+		resize(c, m->wx + oh, m->wy + ov, w - (2*c->bw) - oh - ih / 2, m->wh - (2*c->bw) - (2*ov), False);
 		c = nexttiled(c->next);
-		resize(c, m->wx + w, m->wy, w - (2*c->bw), m->wh - (2*c->bw), False);
+		resize(c, m->wx + w + ih / 2, m->wy + ov, w - (2*c->bw) - oh - ih / 2, m->wh - (2*c->bw) - (2*ov), False);
 	} else {
 		ntop = n / 2;
 		nbottom = n - ntop;
+        cx = m->wx + oh;
 		for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-			if(i < ntop)
-				resize(c, m->wx + i * m->ww / ntop, m->wy, m->ww / ntop - (2*c->bw), m->wh / 2 - (2*c->bw), False);
-			else
-				resize(c, m->wx + (i - ntop) * m->ww / nbottom, m->wy + m->wh / 2, m->ww / nbottom - (2*c->bw), m->wh / 2 - (2*c->bw), False);
+			if(i < ntop) {
+                cw = (m->ww - 2 * oh - (ntop - 1) * ih) / ntop;
+				resize(c, cx, m->wy + ov, cw, m->wh / 2 - (2*c->bw) - ov - iv / 2, False);
+                cx += cw + ih;
+            } else {
+                if (i == ntop) cx = m->wx + oh;
+                cw = (m->ww - 2 * oh - (nbottom - 1) * ih) / nbottom;
+				resize(c, cx, m->wy + m->wh / 2 + iv / 2, cw, m->wh / 2 - (2*c->bw) - ov - iv / 2, False);
+                cx += cw + ih;
+            }
 		}
 	}
 }
