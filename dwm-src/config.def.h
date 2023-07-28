6bf1a1910a86b0ebe31bf8c3a12d12cbaa70065a
FrozenArcher <yhy242989105@hotmail.com>
2023-07-28 21:22:31 +0800

    change terminal to alacritty

--- dwm-src/config.def.h
+++ dwm-src/config.def.h
@@ -107,7 +107,7 @@ static const char *layoutcolors[][2] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *pkillcmd[]  = { "pkill", "dwm", NULL };
 
 static const Key keys[] = {
