ab800cdcda4d2024b5bc3a65a3cdcf5cfc2964ca
FrozenArcher <yhy242989105@hotmail.com>
2023-08-07 02:41:02 +0800

    change dmenu to rofi

--- dwm-src/config.def.h
+++ dwm-src/config.def.h
@@ -106,7 +106,7 @@ static const char *layoutcolors[][2] = {
 #define STATUSBAR "dwmblocks"
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", "-config", "~/.config/rofi/rofi-dmenu.rasi", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *pkillcmd[]  = { "pkill", "dwm", NULL };
 
